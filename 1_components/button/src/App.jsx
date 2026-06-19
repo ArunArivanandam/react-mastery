1dccdcfd2eb834d9b419906a07d9f65f95dc1012
ArunArivanandam <arunarivanandam@gmail.com>
2026-06-19 17:09:05 +0530

    reusable button component created

---
+++ 1_components/button/src/App.jsx
@@ -0,0 +1,29 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div className="p-4 m-5">
+      {/* <Button text="Save" onClick={() => console.log("...Saving")} />
+      <Button text="Submit" onClick={() => console.log("...Submitting")} />
+      <Button text="Delete" onClick={() => console.log("...Deleting")} /> 
+        <Button
+        onClick={() => console.log("...Deleting")}
+        style={{ backgroundColor: "red" }}
+      >*/}
+      <Button onClick={() => console.log("...Saving")} disabled>
+        Save
+      </Button>
+      <Button
+        onClick={() => console.log("...Submitting")}
+        style={{ backgroundColor: "green" }}
+      >
+        Submit
+      </Button>
+      <Button onClick={() => console.log("...Deleting")} className="bg-red-500">
+        Delete
+      </Button>
+    </div>
+  );
+}
+
+export default App;
